a2624449e903e0e7920de682bf9395503ba6e5f1
Hosana <hosana@iMac-de-LAPISCO.local>
2026-08-27 17:32:08 -0300

    terminar em casa com a webcam do meu notebook

---
+++ q15.py
@@ -0,0 +1,3 @@
+#Abrir uma câmera, capturar uma imagem (frame), transforme em tom de cinza, visualizar imagema,
+#de entrada, aplique o filtro de canny e visualize os resultados. Continue infinitamente capturando, 
+#transformando em tom de cinza, aplicando canny e visualizando.
