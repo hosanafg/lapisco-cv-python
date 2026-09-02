8f4f2b3f91bf55659ff0c4e0fba6f493145051c4
Hosana <fernandeshosana48@gmail.com>
2026-09-02 10:09:08 -0300

    add filtro laplaciano

--- q18.py
+++ q18.py
@@ -2,3 +2,16 @@
 Laplaciano, aplique a técnica de Equalização no resultado obtido na detecção das bordas, 
 onde a maior intensidade de borda seja 255, e a menor intensidade da borda seja 0. """
 
+import cv2
+
+image=cv2.imread('output/lagarto.jpeg')
+image_gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+laplace=cv2.Laplacian(image_gray, ddepth=cv2.CV_64F, ksize=3)
+
+laplace=cv2.convertScaleAbs(laplace)
+equalized_laplacian=cv2.equalizeHist(laplace)
+
+cv2.imshow('Input grayscale image', image_gray)
+cv2.imshow('Laplacian filter result', laplace)
+cv2.imshow('Equalized Laplacian', equalized_laplacian)
+cv2.waitKey(0)
