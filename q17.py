d05efa7408c9039e6053a9c55b18d5e2ad95157f
Hosana <fernandeshosana48@gmail.com>
2026-09-02 09:55:20 -0300

    reescrevendo a questao 17

--- q17.py
+++ q17.py
@@ -25,10 +25,51 @@ image_gray=image_to_grayscale(image)
 #image_gray.save("arduino_gray_pil.jpeg")
 image_gray.save("2-arduino_gray_pil.jpeg")
 
-""" #ver o histograma original:
-def get_histograma(img):
-    original_hist = np.zeros([256], np.uint8)
-    
-#eq histograma (img grayscale) 
-def equalizar(img_grayscale):
-    equalized_hist = np.zeros([256], np.uint8) """
+image_np=np.asarray(image_gray)
+flat=image_np.flatten()
+plt.hist(flat,bins=50)
+
+def get_histogram(image_np,bins):
+    histogram=np.zeros(bins)
+    for pixel in image_np:
+        histogram[pixel]+=1
+    return histogram
+
+hist=get_histogram(flat,256)
+
+#plt.plot(hist)
+
+def cum_sum(a):
+    a=iter(a)
+    b=[next(a)]
+
+    for i in a:
+        b.append(b[-1]+i)
+    return np.array(b)
+
+cs=cum_sum(hist)
+#plt.plot(cs)
+
+nj=(cs-cs.min())*255
+N=cs.max()-cs.min()
+cs=nj/N
+#plt.plot(cs)
+
+cs=cs.astype('uint8')
+
+img_new=cs[flat]
+#plt.hist(img_new,bins=50)
+
+img_new=np.reshape(img_new,image_np.shape)
+
+#plotar as imagens
+fig = plt.figure()
+fig.set_figheight(15)
+fig.set_figwidth(15)
+
+fig.add_subplot(1,2,1)
+plt.imshow(image, cmap='gray')
+fig.add_subplot(1,2,2)
+plt.imshow(img_new, cmap='gray')
+
+plt.show(block=True)
